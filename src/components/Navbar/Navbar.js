ebd416b9454877726544f6bf87d8c9fffccb168e
RahulREg <60970896+RahulREg@users.noreply.github.com>
2021-10-05 15:13:11 +0530

    Intial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="navMenu">
+            <a href="tel:00000111222">(00) 000 111 222</a>
+        </div>
+    )
+}
