900d40f822a14d7021566e4c495a373afb5e05f7
RahulREg <60970896+RahulREg@users.noreply.github.com>
2021-10-05 17:05:31 +0530

    Navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { FaTwitter, FaFacebook, FaGooglePlusG, FaInstagram } from 'react-icons/fa';
 
 export const Navbar = () => {
     return (
         <div className="navMenu">
-            <a href="tel:00000111222">(00) 000 111 222</a>
+            <div className="part">
+                <a className="borderright" href="tel:00000111222">(00) 000 111 222</a>
+                <a href="mailto:info@somedomain.com">info@somedomain.com</a>
+            </div>
+            <div className="part">
+                <a href="#"><FaTwitter /></a>
+                <a href="#"><FaFacebook /></a>
+                <a href="#"><FaGooglePlusG /></a>
+                <a className="borderright" href="#"><FaInstagram /></a>
+                <div>USD</div>
+            </div>
         </div>
     )
 }
+
+
